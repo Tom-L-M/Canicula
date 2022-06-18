300e0c7f6611e8bdab62e2ec514115968aa3b608
Tom Luchesi <69174010+Tom-L-M@users.noreply.github.com>
2022-06-18 16:59:38 -0300

    Update namespace.js

--- libs/namespace.js
+++ libs/namespace.js
@@ -1,11 +1,12 @@
-var sentrytimeout;
-var configs = {
-    decoyFilename: "__decoy.txt",
-    decoyContent: "trigger",
-    //decoyCheckInterval: 1000/locations.length
-    decoyCheckInterval: 100,
-    locations: [
-        //"C:/Users/%USERPROFILE%/Documents"
-        "C:\\Users\\%USERPROFILE%\\Desktop\\pride_0.0.4\\test"
-    ]
-}
+var configs = {}
+
+configs.filesentrytimeout = 0,
+configs.procsentrytimeout = 0,
+configs.procsentryInterval = 1000,
+configs.decoyFilename = "__decoy.txt",
+configs.decoyContent = "trigger",
+configs.locations = [
+    //"C:/Users/%USERPROFILE%/Documents"
+    "C:\\Users\\%USERPROFILE%\\Desktop\\pride_0.0.4\\test"
+]
+configs.decoyCheckInterval = (1000/configs.locations.length);
