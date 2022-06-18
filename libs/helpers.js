6e8d5eeb549c16a0897dec308240c6d34b3032ca
Tom Luchesi <69174010+Tom-L-M@users.noreply.github.com>
2022-06-18 16:59:53 -0300

    Update helpers.js

--- libs/helpers.js
+++ libs/helpers.js
@@ -1,57 +1,94 @@
 //TODO: Create a function that uses the Save() method to create a shortcut to the application on the desktop
 // [https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/scripting-articles/k5x59zft(v=vs.84)]
 
+//TODO: declarar as variáveis com nomes dos namespaces no arquivo 'namespace.js' e mover configurações para um arquivo 'config.js' dentro do diretório '/core/'
+
 var Shell = {
-    startEngine: function() { return new ActiveXObject("WScript.Shell"); }
+    startEngine: function () { return new ActiveXObject("WScript.Shell"); }
 }
 
 var File = {
-    startEngine: function() { return new ActiveXObject("Scripting.FileSystemObject"); },
-    formatPath: function(p) { return p.replace('/', '\\'); },
+    startEngine: function () { return new ActiveXObject("Scripting.FileSystemObject"); },
+    formatPath: function (p) { return p.replace('/', '\\'); },
     verifyExistance: function(fpath) {
         var fso = this.startEngine();
         var b = formatPath(fpath);
         var p = b.substring(0, fpath.lastIndexOf('\\'));
         var f = b.substring(fpath.lastIndexOf('\\'));
         return (fso.FolderExists(p) && fso.FileExists(f));
+    }
+}
+
+var View = {
+    loadStatusFrame: function (service, content) {
+        document.getElementById(service + "-status").innerHTML = content;
+        return 0;
     },
-    create: function(name) {},
-    get: function() {},
-    remove: function() {},
-    edit: function() {}
+    loadConfigFrame: function () {
+        var render = "<br/><br/><br/>" + 
+            "Number of decoy files:  " + configs.locations.length + "<br/>" +
+            "Verification interval:  " + configs.decoyCheckInterval + "<br/>" +
+            "Decoy file name:        " + configs.decoyFilename + "<br/>" +
+            "Bytes written per file: " + configs.decoyContent.length * 2 + "<br/>"
+            
+        document.getElementById("config-status").innerHTML = render;
+    },
+    displayServiceStatus: function (opts) {
+        var service = opts.service;
+        var status = opts.status;
+        var elapsed = opts.elapsed;
+        var verif = opts.verif;
+        var content = "" +
+            "<code>" + 
+                " Service Status: " + status + "<br/>" +
+                " Elapsed Time: " + elapsed + "<br/>" +
+                " Verifications: " + verif + "<br/>" +
+            "</code>"
+        View.loadStatusFrame(service, content);
+        return 0;
+    },
+    disableServiceButtons: function (service, button) { //"filesentry", "start"
+        var c = "" + service + "-" + button;
+        document.getElementById(c).disabled = true;
+        return 0;
+    },
+    enableServiceButtons: function (service, button) {
+        var c = "" + service + "-" + button;
+        document.getElementById(c).disabled = false;
+        return 0;
+    }
 }
 
-function displayServiceStatus(opts) {
-    var service = opts.service;
-    var status = opts.status;
-    var elapsed = opts.elapsed;
-    var verif = opts.verif;
-    var content = "" +
-        "<code>" + 
-            " Service Status: " + status + "<br/>" +
-            " Elapsed Time: " + elapsed + "<br/>" +
-            " Decoy Verifications: " + verif + "<br/>" +
-        "</code>"
-    loadStatusFrame(service, content);
-    return 0;
+var Env = {
+    getUser: function () {
+        var wshShell = new ActiveXObject("WScript.Shell");
+        var wshEnv = wshShell.Environment("Process");
+        var home = wshEnv("HOMEPATH");
+        return home;
+    },
+    mountPath: function (path) {
+        var u = Env.getUser()
+        var v = u.substring(u.indexOf("\\", 2) + 1);
+        var n = path.replace("%USERPROFILE%", v);
+        return n;
+    }
 }
 
-function getUser() {
-    var wshShell = new ActiveXObject("WScript.Shell");
-    var wshEnv = wshShell.Environment("Process");
-    var home = wshEnv("HOMEPATH");
-    return home;
+var Sys = {
+    killProcess: function (processId, processName) {
+        if (confirm('Are you sure you want to terminate the process "' + processName + '"?')) {
+            var WshShell = new ActiveXObject("WScript.Shell");
+            var oExec = WshShell.Run("taskkill /F /PID " + processId, 0, true);
+            getSysRunningProcesses()
+        }
+    }
 }
 
-function mountPath(path) {
-    var u = getUser()
-    var v = u.substring(u.indexOf("\\", 2) + 1);
-    var n = path.replace("%USERPROFILE%", v);
-    return n;
-}
 
+// TODO: Migrate methods below to respective scripts (*utils: procSentryUtils / triggerUtils / fileSentryUtils) and create namespaces for it
 function emergencyTrigger() {
-    alert("Missing File Decoys");
+    
+    alert("alert");
     return 0;
 }
 
@@ -67,7 +104,7 @@ function deleteDecoyFiles(locs) {
     var fso = File.startEngine();
     var counter = 0;
     for (var i = 0; i < locs.length; i++) {
-        var pa = mountPath(locs[i]);
+        var pa = Env.mountPath(locs[i]);
         if (fso.FolderExists(pa)) {
             var file = pa + "\\"+ configs.decoyFilename;
             if (!fso.FileExists(file)) { counter++; continue; }
@@ -75,6 +112,8 @@ function deleteDecoyFiles(locs) {
             act.Delete();
         }
     }
+    if (counter != 0) { alert("WARNING:" + counter + " decoy files were not found"); }
+    
     return counter;
 }
 
@@ -83,7 +122,7 @@ function createDecoyFiles(locs, content) {
     var tf;
     var fso = File.startEngine();
     for (var i = 0; i < locs.length; i++) {
-        var pa = mountPath(locs[i]);
+        var pa = Env.mountPath(locs[i]);
         if (fso.FolderExists(pa)) {
             tf = fso.CreateTextFile(pa + "\\"+ configs.decoyFilename, true);
             tf.Write(content);
