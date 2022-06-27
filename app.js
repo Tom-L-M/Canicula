e845c7dbf6a9b5d2dc4cc72dc4553d0319f251df
Tom Luchesi <69174010+Tom-L-M@users.noreply.github.com>
2022-06-27 00:43:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var Env = Import('./core/env.js');               // ; [eng]           ; Environm
 var Sys = Import('./core/sys.js');               // ; [eng]           ; System               ; System custom methods
 var Trg = Import('./core/trg.js');               // ; []              ; Trigger              ; Application triggers to kill processes
 
-//TODO: remove test dependencies
+//TODO: remove test and experimental dependencies
 var TEST = Import('./test/testlog.js');
 
 var ProcSUtils = Import('./func/procsutils.js'); // ; [cfg]           ; ProcSentry utils     ; 
@@ -22,6 +22,6 @@ Eng.StartAppEngines();
 //TODO: create a function that verifies the software integrity before starting, getting the hash of all files, and comparing with an hash kept in a file
 //TODO: Create a function that uses the Save() method to create a shortcut to the application on the desktop
 // [https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/scripting-articles/k5x59zft(v=vs.84)]
-//TODO: Verificar se existe mais de um drive (buscar pelo 'D') e se houver, mapear um certo número de locais para posicionar decoys, de forma a proteger todos os drives locais (remotos não)
+//TODO: Verificar se existe mais de um drive (buscar pelo 'D' e 'E') e se houver, mapear um certo número de locais para posicionar decoys, de forma a proteger todos os drives locais (remotos não)
 
 //TODO BUG : O contador do filesentry continua a contagem se for iniciado e encerrado antes do intervalo (3 segundos)
