f059dfe2dc5dc3fe565ca2f2b84fdc9f5d273246
Tom Luchesi <69174010+Tom-L-M@users.noreply.github.com>
2022-07-08 23:37:48 -0300

    Update testlog.js

--- test/testlog.js
+++ test/testlog.js
@@ -1,35 +1,46 @@
 (function () {
     var TEST = {}
 
+    TEST.storage = "";
+
     TEST.begin = function () {
         var local = Env.getWorkingDir() + "\\test\\log\\";
         return [Eng.Fso, local];
     }
 
-    TEST.log = function (engine, loc, proc) {
-        var tf = engine.CreateTextFile(loc + proc.ProcessId + 'deb.txt', true);
-            tf.Write(
-                "{ " +
-                "   \n Description: " + proc.Description + 
-                "   \n Exe Path: " + proc.ExecutablePath + 
-                "   \n Name: " + proc.Name +
-                "   \n Other Operation Count: " + proc.OtherOperationCount +
-                "   \n Other Transfer Count:" + proc.OtherTransferCount +
-                "   \n Parent Process Id: " + proc.ParentProcessId +
-                "   \n Peak Virtual Size: " + proc.PeakVirtualSize +
-                "   \n Peak Working Set Size: " + proc.PeakWorkingSetSize +
-                "   \n Priority: " + proc.Priority +
-                "   \n Process Id: " + proc.ProcessId +
-                "   \n Read Operation Count: " + proc.ReadOperationCount +
-                "   \n Read Transfer Count: " + proc.ReadTransferCount +
-                "   \n Thread Count: " + proc.ThreadCount +
-                "   \n Virtual Size: " + proc.VirtualSize +
-                "   \n WorkingSet Size: " + proc.WorkingSetSize +
-                "   \n Write Operation Count: " + proc.WriteOperationCount +
-                "   \n Write Transfer Count" + proc.WriteTransferCount + 
-                "\n} "
-            );
-            tf.Close();
+    TEST.log = function(proc) {
+        //var tf = engine.CreateTextFile(loc + proc.ProcessId + 'deb.txt', true);
+            //tf.Write(
+                TEST.storage += (
+                    "{ " +
+                    "   \n Description: " + proc.Description + 
+                    "   \n Exe Path: " + proc.ExecutablePath + 
+                    "   \n Name: " + proc.Name +
+                    "   \n Other Operation Count: " + proc.OtherOperationCount +
+                    "   \n Other Transfer Count:" + proc.OtherTransferCount +
+                    "   \n Parent Process Id: " + proc.ParentProcessId +
+                    "   \n Peak Virtual Size: " + proc.PeakVirtualSize +
+                    "   \n Peak Working Set Size: " + proc.PeakWorkingSetSize +
+                    "   \n Priority: " + proc.Priority +
+                    "   \n Process Id: " + proc.ProcessId +
+                    "   \n Read Operation Count: " + proc.ReadOperationCount +
+                    "   \n Read Transfer Count: " + proc.ReadTransferCount +
+                    "   \n Thread Count: " + proc.ThreadCount +
+                    "   \n Virtual Size: " + proc.VirtualSize +
+                    "   \n WorkingSet Size: " + proc.WorkingSetSize +
+                    "   \n Write Operation Count: " + proc.WriteOperationCount +
+                    "   \n Write Transfer Count" + proc.WriteTransferCount + 
+                    "\n} "
+                )
+            //);
+            //tf.Close();
+        return;
+    }
+
+    TEST.write = function(engine, loc) {
+        var tf = engine.CreateTextFile(loc + 'deb.txt', true);
+        tf.Write(TEST.storage);
+        tf.Close();
         return;
     }
 
