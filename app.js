d7347df111eb5fa21778d267c9c5bdf9bdf6f777
Tom Luchesi <69174010+Tom-L-M@users.noreply.github.com>
2022-07-08 23:20:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 var Import = function (f) { return eval((new ActiveXObject("Scripting.FileSystemObject")).OpenTextFile(f, 1).ReadAll()); } //This is needed to import other modules
 
 // Module (path reference is 'app.hta')          // # Dependencies    # Name                 # Description
+
+// Zero-Level Modules (zero dependencies)
 var Eng = Import('./core/eng.js');               // ; []              ; Engines              ; Includes ActiveX Engine objects (Shell and Fso)
 var Cfg = Import('./core/cfg.js');               // ; []              ; Configs              ; Configurations file
+var Trg = Import('./core/trg.js');               // ; []              ; Trigger              ; Application triggers to kill processes
+
+//Low-Level Modules (core-only dependencies)
 var Vws = Import('./core/vws.js');               // ; [cfg]           ; App View             ; App views rendering custom methods
 var Fso = Import('./core/fso.js');               // ; [eng]           ; FileSysObject        ; FileSystemObject custom methods
 var Env = Import('./core/env.js');               // ; [eng]           ; Environment          ; Environment custom methods
 var Sys = Import('./core/sys.js');               // ; [eng]           ; System               ; System custom methods
-var Trg = Import('./core/trg.js');               // ; []              ; Trigger              ; Application triggers to kill processes
 
-//TODO: remove test and experimental dependencies
+//Low-Level library Modules
+var Crypto = Import('./libs/crypto.js');         // ; []              ; Crypto               ; Namespace for SHA-256 hashing and criptographic resources
+
+
+// Test-Level Modules (experimental)
 var TEST = Import('./test/testlog.js');
+//TODO: remove test and experimental dependencies
 
-var ProcSUtils = Import('./func/procsutils.js'); // ; [cfg]           ; ProcSentry utils     ; 
-var FileSUtils = Import('./func/filesutils.js'); // ; [cfg,eng,env]   ; FileSentry utils     ; 
-var ProcSentry = Import('./func/procsentry.js'); // ; [cfg,trg,vws]   ; ProcSentry manager   ; 
-var FileSentry = Import('./func/filesentry.js'); // ; [cfg,trg,vws]   ; FileSentry manager   ; 
-
-Eng.StartAppEngines();
+// High-Level Modules (many dependencies)
+var ProcSUtils = Import('./func/procsutils.js'); // ; [cfg,eng]       ; ProcSentry utils
+var FileSUtils = Import('./func/filesutils.js'); // ; [cfg,eng,env]   ; FileSentry utils 
+var ProcSentry = Import('./func/procsentry.js'); // ; [cfg,trg,vws]   ; ProcSentry manager 
+var FileSentry = Import('./func/filesentry.js'); // ; [cfg,trg,vws]   ; FileSentry manager 
 
 //TODO: create a function that verifies the software integrity before starting, getting the hash of all files, and comparing with an hash kept in a file
 //TODO: Create a function that uses the Save() method to create a shortcut to the application on the desktop
