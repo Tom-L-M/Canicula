0907d86c3daaeb10748fe5930d4c6e0064a33da9
Tom Luchesi <69174010+Tom-L-M@users.noreply.github.com>
2022-07-12 01:31:18 -0300

    Add files via upload

--- test/testlog.js
+++ test/testlog.js
@@ -1,7 +1,7 @@
 (function () {
     var TEST = {}
 
-    TEST.storage = "";
+    TEST.storage = "Name,Description,Executable Path,PID,Parent PID,Priority,Threads,Read Op. Count,Read Op. Vol.,Write Op. Count,Write Op. Vol.,Other Op. Count,Other Op. Vol.,Virtual Size,Peak Virtual Size,Working Set,Peak Working Set";
 
     TEST.begin = function () {
         var local = Env.getWorkingDir() + "\\test\\log\\";
@@ -11,26 +11,46 @@
     TEST.log = function(proc) {
         //var tf = engine.CreateTextFile(loc + proc.ProcessId + 'deb.txt', true);
             //tf.Write(
+                // TEST.storage += (
+                //     "{ " +
+                //     "   \n Description: " + proc.Description + 
+                //     "   \n Exe Path: " + proc.ExecutablePath + 
+                //     "   \n Name: " + proc.Name +
+                //     "   \n Other Operation Count: " + proc.OtherOperationCount +
+                //     "   \n Other Transfer Count:" + proc.OtherTransferCount +
+                //     "   \n Parent Process Id: " + proc.ParentProcessId +
+                //     "   \n Peak Virtual Size: " + proc.PeakVirtualSize +
+                //     "   \n Peak Working Set Size: " + proc.PeakWorkingSetSize +
+                //     "   \n Priority: " + proc.Priority +
+                //     "   \n Process Id: " + proc.ProcessId +
+                //     "   \n Read Operation Count: " + proc.ReadOperationCount +
+                //     "   \n Read Transfer Count: " + proc.ReadTransferCount +
+                //     "   \n Thread Count: " + proc.ThreadCount +
+                //     "   \n Virtual Size: " + proc.VirtualSize +
+                //     "   \n WorkingSet Size: " + proc.WorkingSetSize +
+                //     "   \n Write Operation Count: " + proc.WriteOperationCount +
+                //     "   \n Write Transfer Count: " + proc.WriteTransferCount + 
+                //     "\n} "
+                // )
                 TEST.storage += (
-                    "{ " +
-                    "   \n Description: " + proc.Description + 
-                    "   \n Exe Path: " + proc.ExecutablePath + 
-                    "   \n Name: " + proc.Name +
-                    "   \n Other Operation Count: " + proc.OtherOperationCount +
-                    "   \n Other Transfer Count:" + proc.OtherTransferCount +
-                    "   \n Parent Process Id: " + proc.ParentProcessId +
-                    "   \n Peak Virtual Size: " + proc.PeakVirtualSize +
-                    "   \n Peak Working Set Size: " + proc.PeakWorkingSetSize +
-                    "   \n Priority: " + proc.Priority +
-                    "   \n Process Id: " + proc.ProcessId +
-                    "   \n Read Operation Count: " + proc.ReadOperationCount +
-                    "   \n Read Transfer Count: " + proc.ReadTransferCount +
-                    "   \n Thread Count: " + proc.ThreadCount +
-                    "   \n Virtual Size: " + proc.VirtualSize +
-                    "   \n WorkingSet Size: " + proc.WorkingSetSize +
-                    "   \n Write Operation Count: " + proc.WriteOperationCount +
-                    "   \n Write Transfer Count" + proc.WriteTransferCount + 
-                    "\n} "
+                    "\n" 
+                    + proc.Name +','
+                    + proc.Description + ','
+                    + proc.ExecutablePath + ','
+                    + proc.ProcessId +','
+                    + proc.ParentProcessId +','
+                    + proc.Priority +','
+                    + proc.ThreadCount +','
+                    + proc.ReadOperationCount +','
+                    + proc.ReadTransferCount +','
+                    + proc.WriteOperationCount +','
+                    + proc.WriteTransferCount + ','
+                    + proc.OtherOperationCount +','
+                    + proc.OtherTransferCount +','
+                    + proc.VirtualSize +','
+                    + proc.PeakVirtualSize +','
+                    + proc.WorkingSetSize +','
+                    + proc.PeakWorkingSetSize
                 )
             //);
             //tf.Close();
